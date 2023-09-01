b36d3e0c4203cce20ccbe99f11da2fa176e953c7
Sasini2000 <91366574+Sasini2000@users.noreply.github.com>
2023-09-01 14:40:05 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,18 +6,18 @@ import torchvision
 
 class ResNet(nn.Module):
     """encoder + classifier"""
-    def __init__(self, name='resnet50', num_classes=2):
+    def __init__(self, name='resnet152', num_classes=2):
         super(ResNet, self).__init__()
-        if (name == 'resnet50'):
+        if (name == 'resnet152'):
+            self.encoder = torchvision.models.resnet152(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(4096, num_classes)
+        else:
             self.encoder = torchvision.models.resnet50(zero_init_residual=True)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(2048, num_classes)
-        else:
-            self.encoder = torchvision.models.resnet18(zero_init_residual=True)
-            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-            self.encoder.fc = nn.Identity()
-            self.fc = nn.Linear(512, num_classes)
     def forward(self, x):
 
         return self.fc(self.encoder(x))
